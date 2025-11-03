ffb6c5309b6a2e9234d1eabcc2b82799185514bb
David Malchin <malchin459@gmail.com>
2025-11-03 07:38:13 +0200

    Add utility API types

--- packages/bot/src/interactions/index.ts
+++ packages/bot/src/interactions/index.ts
@@ -2,3 +2,5 @@ export * from './data/index.ts';
 export * from './extensions/index.ts';
 export * from './handler.ts';
 export * from './interaction.ts';
+export * from './utils/index.ts';
+export * from './values.ts';
